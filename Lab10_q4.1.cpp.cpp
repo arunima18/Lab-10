8accb150235d79ceacb57dcb5bc9bc6d39f4e04e
arunima18 <42172417+arunima18@users.noreply.github.com>
2018-11-18 01:59:26 +0530

    Add files via upload

---
+++ Lab10_q4.1.cpp.cpp
@@ -0,0 +1,142 @@
+/* Using the program point.cpp, add a member function print to the class Point so that it prints out coordinates as: 
+    (2, 3)
+for a point with coordinates (2, 3). Test it in main(). */
+
+#include<iostream>
+using namespace std;
+
+
+
+
+// ********************************************************************
+//  Declaration of class Point
+// ********************************************************************
+
+// class Point represents a two-dimensional point
+class Point
+{
+public:
+  // default class constructor (with no arguments):
+  Point(); 
+
+  // class constructor that sets the coordinates x, y to the values xval,
+  // yval:
+  Point(int xval, int yval);
+
+  // member function for moving a point by dx, dy:
+  void Move(int dx, int dy);
+
+  // member functions for getting values of x, y:
+  int Get_X() const;
+  int Get_Y() const;
+
+  // member functions for setting x, y to xval, yval respectively  
+  void Set_X(int xval);
+  void Set_Y(int yval);
+
+//Lab 7 exercise 4.1. Add declaration of member function Print here:
+
+// private data members x, y represent coordinates of the point:
+private:
+  int X;
+  int Y;
+};  
+
+
+// ********************************************************************
+//  Methods for class Point
+// ********************************************************************
+
+// class constructor sets X, Y to zero when no values are specified:
+Point::Point()
+{
+  X = 0;
+  Y = 0;
+}
+
+// class constructor sets X, Y to given values xval, yval:
+Point::Point(int xval, int yval)
+{
+  X = xval;
+  Y = yval;
+}
+
+// member function Move: increases the x coordinate by dx and the y
+// coordinate by dy.
+void Point::Move(int dx, int dy)
+{
+  X += dx;
+  Y += dy;
+}
+
+// Get_X returns the value of the X coordinate
+int Point::Get_X() const
+{
+  return X;
+}
+
+// Get_Y returns the value of the Y coordinate
+int Point::Get_Y() const
+{
+  return Y;
+}
+
+// Set_X sets the value of X coordinate to xval
+void Point::Set_X(int xval)
+{
+  X = xval;
+} 
+
+// Set_Y sets the value of Y coordinate to yval
+void Point::Set_Y(int yval)
+{
+  Y = yval;
+} 
+
+//Member function to print the values of X and Y
+void Point::Printval(){
+    cout<<"The x coordinate is "<<Get_X()<<endl;
+    cout<<"The y coordinate is "<<Get_Y()<<endl;
+}
+
+    
+
+
+
+
+// ********************************************************************
+//  main() function for testing classes Point and Rectangle
+// ********************************************************************
+
+
+
+// Testing classes Point and Rectangle
+int main()
+{
+// Declaring a point using default class constructor (x = y = 0):
+  Point p1;
+  cout<< "The x value for p1 is " << p1.Get_X() << endl;
+  cout<< "The y value for p1 is " << p1.Get_Y() << endl;
+
+// Declaring a point with coordinates X = 2, Y = 3:
+  Point p2(2, 3);
+  cout<< "The x value for p2 is " << p2.Get_X() << endl;
+  cout<< "The y value for p2 is " << p2.Get_Y() << endl;
+
+// Moving point p2 by (1, -1):
+  p2.Move(1, -1);
+  cout<< "After the move:" << endl;
+  cout<< "The x value for p2 is " << p2.Get_X() << endl;
+  cout<< "The y value for p2 is " << p2.Get_Y() << endl;
+
+//Lab 7 exercise 4.1. Test member function print on points p1, p2:
+
+//Lab 7 Exercises 4.2, 4.3. Testing of the class Rectangle goes here:  
+
+  return 0;
+}
+
+//---------------------- point.cpp ------------------------------------//
+
+
+
